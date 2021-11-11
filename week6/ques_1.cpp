f06e1694046006b1416ef16887ec56cc04a759c5
Anjali Pathak <anjalipathak016@gmail.com>
2021-11-11 09:53:21 +0530

    added

--- week6/ques_1.cpp
+++ week6/ques_1.cpp
@@ -1,48 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool checkPath(int node,vector<int>adj[],int s,int d,vector<int>&vis){
-	vis[node]=1;
-	for(auto it : adj[s]){
-		if(it==d)
-			return true;
 
-		if(!vis[it]){
-			vis[it]=1;
-			if(checkPath(it,adj,s,d,vis))
-				return true;
-		}
-	}
-	return false;
+bool bfs(vector<vector<int> > g, int v)
+{
+    vector<int> color(v, -1);
+    queue<int> q;
+    q.push(0);
+    color[v] = 1;
+    while (!q.empty())
+    {
+        int temp = q.front();
+        q.pop();
+        if (g[temp][temp] == 1)
+            return false;
+       
+        for (int i = 0; i < v; i++)
+        {
+            if (g[temp][i] && color[i] == -1)
+            {
+                color[i] = 1 - color[temp];
+                q.push(i);
+            }
+            else if (g[temp][i] && color[i] == color[temp])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
-bool isPathDFS(int n,vector<int>adj[],int s,int d){
-	if(s==d) return true;
-	vector<int>vis(n+1,0);
 
-	for(int i=1;i<=n;i++){
-		if(checkPath(i,adj,s,d,vis))
-			return true;
-	}
-	return false;
-}
-int main(){
-	int n;
-	int m;
-	cin >> n >> m;
-	vector<int>adj[n+1];
-	for(int i=0;i<m;i++)
-	{
-		int u,v;
-		cin>>u>>v;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-	}
-	int s,d;
-	cin >> s >> d;
-	if(isPathDFS(n,adj,s,d))
-		cout << "True" << endl;
-	else
-		cout << "False" << endl;
-
-
-	return 0;
+int main()
+{
+    int v, e;
+    cin >> v >> e;
+    vector<vector<int> > g(v, vector<int>(v, 0));
+    for (int i = 0; i < e; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        x--, y--;
+        g[x][y] = 1;
+        g[y][x] = 1;
+    }
+    if (bfs(g, v))
+    {
+        cout << "Yes Barpartite";
+    }
+    else
+    {
+        cout << "Not Barpartite";
+    }
+    return 0;
 }
